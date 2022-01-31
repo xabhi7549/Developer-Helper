f81294bbdc2b2aa4e693fb2843c19d5250243b99
Abhinav Maurya <xabhi7549@gmail.com>
2022-01-31 15:24:32 +0530

    DebugLogger

--- X-SDK/src/main/java/com/site_valley/x_sdk/XLogger.kt
+++ X-SDK/src/main/java/com/site_valley/x_sdk/XLogger.kt
@@ -1,17 +1,34 @@
+/*
+ * Created by abhinav for ETAOP Pvt. Ltd.
+ * Copyright (c) 2022 . All rights reserved.
+ * Last Updated on 31/01/22, 3:24 PM
+ */
+
 package com.site_valley.x_sdk
 
 import android.util.Log
 import com.google.gson.Gson
 
 object XLogger {
-    fun logE(any: Any){
 
+    /**
+     * Logs Always
+     * @param any Any Object Or String
+     *
+     */
+    fun logE(any: Any) {
         try {
-            Log.e("XLogger:: ",Gson().toJson(any))
-        }catch (e:Exception){
-            Log.e("XLogger Error:: ",e.localizedMessage.toString())
+            Log.e("XLogger:: ", Gson().toJson(any))
+        } catch (e: Exception) {
+            Log.e("XLogger Error:: ", e.localizedMessage.toString())
         }
-
     }
+
+    fun logOnDebugE(any: Any) {
+        if (BuildConfig.DEBUG) {
+            logE(any)
+        }
+    }
+
 
 }
