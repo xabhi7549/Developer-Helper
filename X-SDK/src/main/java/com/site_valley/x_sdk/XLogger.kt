f4c53dcd55ec42cba97ff79f6a443503448bf706
Abhinav Maurya <xabhi7549@gmail.com>
2022-02-05 21:00:26 +0530

    Key Value Log

--- X-SDK/src/main/java/com/site_valley/x_sdk/XLogger.kt
+++ X-SDK/src/main/java/com/site_valley/x_sdk/XLogger.kt
@@ -1,7 +1,7 @@
 /*
  * Created by abhinav for ETAOP Pvt. Ltd.
  * Copyright (c) 2022 . All rights reserved.
- * Last Updated on 31/01/22, 4:04 PM
+ * Last Updated on 05/02/22, 9:00 PM
  */
 
 package com.site_valley.x_sdk
@@ -18,6 +18,19 @@ object XLogger {
     fun logE(any: Any) {
         try {
             Log.e("XLogger:: ", XObjects.getStringFromObject(any).toString())
+        } catch (e: Exception) {
+            Log.e("XLogger Error:: ", e.localizedMessage.toString())
+        }
+    }
+
+    /**
+     * Logs Always
+     * @param any Any Object Or String
+     *
+     */
+    fun logE(key: String, any: Any) {
+        try {
+            Log.e("XLogger:: $key :", XObjects.getStringFromObject(any).toString())
         } catch (e: Exception) {
             Log.e("XLogger Error:: ", e.localizedMessage.toString())
         }
