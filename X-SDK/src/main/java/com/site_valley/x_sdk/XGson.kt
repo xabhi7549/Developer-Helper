735446690dfbd7b6d996490ef340b278cce990ac
Abhinav <xabhi7549@gmail.com>
2022-02-11 00:51:37 +0530

    Tag Updated 2

--- X-SDK/src/main/java/com/site_valley/x_sdk/XGson.kt
+++ X-SDK/src/main/java/com/site_valley/x_sdk/XGson.kt
@@ -1,7 +1,7 @@
 /*
  * Created by abhinav for ETAOP Pvt. Ltd.
  * Copyright (c) 2022 . All rights reserved.
- * Last Updated on 10/02/22, 8:32 PM
+ * Last Updated on 11/02/22, 12:51 AM
  */
 
 package com.site_valley.x_sdk
@@ -54,4 +54,50 @@ jobs:
           dependencies-cache-enabled: true
           configuration-cache-enabled: true
           arguments: assembleRelease
+          *
+          *
+          * apk:
+    name: Generate APK
+    runs-on: ubuntu-latest
+
+    steps:
+      - uses: actions/checkout@v1
+      - name: set up JDK 11
+        uses: actions/setup-java@v1
+        with:
+          java-version: 11
+      - name: Build debug APK
+        run: bash ./gradlew assembleDebug --stacktrace
+      - name: Create Release Build
+        id: signArtifact
+        uses: r0adkll/sign-android-release@v1.0.1
+        with:
+          releaseDirectory: app/build/outputs/apk/ourbuildtype/release
+          alias: ${{ secrets.KEYALIAS }}
+          signingKeyBase64: ${{ secrets.KEYSTORE }}
+          keyStorePassword: ${{ secrets.KEYSTORE_PASSWORD }}
+          keyPassword: ${{ secrets.KEY_PASSWORD }}
+      - name: Github Sec
+        run: echo ${{ secrets.GITHUB_TOKEN }}
+      - name: Github Data
+        run: echo ${{ github.event.release }}
+      - name: Github Event
+        run: echo ${{ github.event }}
+      - name: Github ENV
+        run: echo ${{ github.env.test }}
+      - name: Upload our APK
+        uses: actions/upload-artifact@v2.2.0
+        with:
+          name: Release artifact
+          path: app/build/outputs/apk/ourbuildtype/release/*.apk
+      - name: Create Release For SDK
+        id: create_release
+        uses: actions/create-release@v1
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+        with:
+          tag_name: ${{ github.event.release.tag_name }}
+          release_name: Release ${{ github.event.release.tag_name }}
+          draft: false
+          prerelease: false
 */
