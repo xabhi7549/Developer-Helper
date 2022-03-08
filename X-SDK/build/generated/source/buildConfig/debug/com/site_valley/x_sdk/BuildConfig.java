783891d19f311aff947f1f47526a8ce821af881a
Abhinav Maurya <xabhi7549@gmail.com>
2022-03-08 16:36:34 +0530

    j,zjhjsha,

---
+++ X-SDK/build/generated/source/buildConfig/debug/com/site_valley/x_sdk/BuildConfig.java
@@ -0,0 +1,16 @@
+/*
+ * Created by abhinav for ETAOP Pvt. Ltd.
+ * Copyright (c) 2022 . All rights reserved.
+ * Last Updated on 08/03/22, 3:21 PM
+ */
+
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.site_valley.x_sdk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.site_valley.x_sdk";
+  public static final String BUILD_TYPE = "debug";
+}
