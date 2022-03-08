f88a5811f5950733ebb6e1e4681ab166c2e9e580
Abhinav Maurya <xabhi7549@gmail.com>
2022-03-08 15:37:31 +0530

    YML

---
+++ app/build/generated/source/buildConfig/debug/com/site_valley/developerhelper/BuildConfig.java
@@ -0,0 +1,18 @@
+/*
+ * Created by abhinav for ETAOP Pvt. Ltd.
+ * Copyright (c) 2022 . All rights reserved.
+ * Last Updated on 08/03/22, 3:23 PM
+ */
+
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.site_valley.developerhelper;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.site_valley.developerhelper";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "2.0.1";
+}
